09c0f2565e8f6e175bb05a7242e1abd77862023d
anwin-kp <anwin.kp.10@gmail.com>
2022-10-16 12:35:11 +0530

    My Routing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { IndexComponent } from './index/index.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+const appRoute: Routes = [
+  {
+    path: '', component: IndexComponent
+  },
+  {
+    path: 'HOME', component: IndexComponent
+  },
+  {
+    path: 'CONTACT', component: ContactComponent
+  },
+  {
+    path: 'SERVICES', component: ServicesComponent
+  },
+  {
+    path: 'ABOUT', component: AboutComponent
+  },
 
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
